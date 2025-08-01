4bb94763be9d38dc02f7931b1558bb9d0c6670e6
Handras18 <bandi200110@gmail.com>
2025-08-01 11:33:53 +0200

    Forgot/Reset password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import { useAuthStore } from "./store/authStore";
 import { useEffect } from "react";
 import DashboardPage from "./pages/DashboardPage";
 import LoadingSpinner from "./components/LoadingSpinner";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuthStore();
@@ -86,6 +88,22 @@ const App = () => {
           }
         ></Route>
         <Route path="/verify-email" element={<EmailVerificationPage />}></Route>
+        <Route
+          path="/forgot-password"
+          element={
+            <RedirectAuthemticatedUser>
+              <ForgotPasswordPage />
+            </RedirectAuthemticatedUser>
+          }
+        ></Route>
+        <Route
+          path="/reset-password/:token"
+          element={
+            <RedirectAuthemticatedUser>
+              <ResetPasswordPage />
+            </RedirectAuthemticatedUser>
+          }
+        ></Route>
       </Routes>
       <Toaster />
     </div>
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+
 const API_URL = "http://localhost:5000/api/auth";
 axios.defaults.withCredentials = true;
 
@@ -9,6 +10,7 @@ export const useAuthStore = create((set) => ({
   error: null,
   isLoading: false,
   isCheckingAuth: true,
+  message: null,
 
   signup: async (email, password, name) => {
     set({ isLoading: true, error: null });
@@ -101,6 +103,39 @@ export const useAuthStore = create((set) => ({
         error: null,
         isCheckingAuth: false,
         isAuthenticated: false,
+      });
+      throw error;
+    }
+  },
+  forgotPassword: async (email) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/forgot-password`, {
+        email,
+      });
+      set({ message: response.data.message, isLoading: false });
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: error.response.data.message || "Error forgotPassword email",
+      });
+      throw error;
+    }
+  },
+  resetPassword: async (token, password) => {
+    set({
+      isLoading: true,
+      error: null,
+    });
+    try {
+      const response = await axios.post(`${API_URL}/reset-password/${token}`, {
+        password,
+      });
+      set({ message: response.data.message, isLoading: false });
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: error.response.data.message || "Error reseting password",
       });
       throw error;
     }
